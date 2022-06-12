6cc99e4ed35d805839834da40efc3489dd0e5a34
Jobsidney <jobsidney67@gmail.com>
2022-06-12 19:55:00 +0300

    commit tree 01677dbaf2c8e29eac01840340a2286901276216

--- index.js
+++ index.js
@@ -63,6 +63,15 @@ const record = [
 
 
 let superbowlWin=function(superbowlTeam){
-    superbowlTeam=array=>array.find(item => item.result==="W");
-    return superbowlTeam(record).result==="W"?superbowlTeam(record).year:'undefined';}
+    let winner=superbowlTeam.find(item => item.result==="W");
+    return winner?winner.year:undefined;}
 console.log(superbowlWin(record))
+// function superbowlWin(superRecord){
+//   const user=superRecord.find(item=>item.result==="W");
+//   if(user){
+//     return user.year;
+//   }else{
+//     return undefined;
+//   }
+// }
+// superbowlWin(record);
